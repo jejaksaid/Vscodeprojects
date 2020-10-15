e98a28d0f4bae9dc7ef10ee302dbbe036615b1bb
jejaksaid <mail@saidramadhan.com>
2020-10-15 22:43:38 +0700

    1510

---
+++ quiz-app/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'Where is Indonesia?',
+        a:  'Asia',
+        b:  'Europe',
+        c:  'America',
+        d:  'Africa',
+        correct: 'a'
+    }, {
+        question: 'What is Pancasila?',
+        a:  'Dasar negara',
+        b:  'Lambang negara',
+        c:  'Burung garuda',
+        d:  'Semua benar',
+        correct: 'a'
+    }, {
+        question: 'How old said?',
+        a:  '22',
+        b:  '23',
+        c:  '14',
+        d:  '19',
+        correct: 'b'
+    }, {
+        question: 'What is best programming language',
+        a:  'Java',
+        b:  'Python',
+        c:  'C#',
+        d:  'Javascript',
+        correct: 'a'
+    }, {
+        question: 'Whos is invent linux?',
+        a:  'Linux torvalds',
+        b:  'Steve jobs',
+        c:  'Bill gates',
+        d:  'Donald trump',
+        correct: 'a'
+    }
+]
